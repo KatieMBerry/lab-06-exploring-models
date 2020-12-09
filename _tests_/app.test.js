ac7244f0e53036db9be42db39d600bc8978369ac
Katie <katieberrym@gmail.com>
2020-12-08 16:34:01 -0800

    added app.test and app.js files; initial hookup test pass

---
+++ _tests_/app.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('app tests', () => {
+    it('responds with hello', () => {
+        return request(app)
+            .get('/')
+            .then(res => {
+                expect(res.text).toEqual('hello')
+            });
+    });
+});
