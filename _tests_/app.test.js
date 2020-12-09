025eef6f6d7ba783a4265f958d30a9610d340fb4
Katie <katieberrym@gmail.com>
2020-12-08 18:03:08 -0800

    added post route and related test - test pass

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,12 +1,36 @@
+require('dotenv').config();
 const request = require('supertest');
 const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
+const fs = require('fs');
+const Customer = require('../lib/models/Customer')
+
 
 describe('app tests', () => {
-    it('responds with hello', () => {
-        return request(app)
-            .get('/customers')
-            .then(res => {
-                expect(res.text).toEqual('hello')
+    beforeEach(() => {
+        return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+    });
+
+    afterEach(() => {
+        return pool.end();
+    });
+
+    it('creates a customer to the database via POST, and returns it', async () => {
+        const response = await request(app)
+            .post('/customers')
+            .send({
+                last_name: 'Nucera',
+                first_name: 'Marco',
+                age: 35,
+                email: 'marco@gmail.com'
             });
+
+        expect(response.body).toEqual({
+            id: '1',
+            last_name: 'Nucera',
+            first_name: 'Marco',
+            age: 35,
+            email: 'marco@gmail.com'
+        });
     });
 });
